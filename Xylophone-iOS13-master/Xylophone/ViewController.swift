d39f5e9ed14a65022910819b00235aaa347a131a
Oversparkling <nicholasongck@gmail.com>
2021-09-14 18:38:55 +0800

    Xylophone app

---
+++ Xylophone-iOS13-master/Xylophone/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+import AVFoundation
+
+var player: AVAudioPlayer?
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+ 
+    @IBAction func keyPresed(_ sender: UIButton) {
+        print(sender.currentTitle)
+        playSound(name: sender.currentTitle)
+    }
+    
+    func playSound(name: String) {
+            let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player!.play()    }
+}
+
